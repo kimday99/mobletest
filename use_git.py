65d2a82676d36befe5df4ccdb37fade4c47d0213
kimday99 <wnqktmvb11@gmail.com>
2023-10-23 17:48:45 +0900

    first

---
+++ use_git.py
@@ -0,0 +1,9 @@
+##git 사용법
+# git init  # 초기화
+# git add .  # 코드 전부 추가
+# git commit -m ""  commit과 어떤 작업 했는지  git commit -m "first commit"
+# git remote add origin (레포지토리 주소)
+# git push origin master (master라는 branch명) #commit 한 것들이 전부 master로 저장
+# git add.
+# git commit -m ""
+# git push origin master
